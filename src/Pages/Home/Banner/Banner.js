1f8473c5f29ed943614c3ff217d13155666f6f9e
alnomancs <alnoman.cs@gmail.com>
2022-04-18 16:14:47 +0100

    fix banner title and description

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -16,16 +16,14 @@ const Banner = () => {
       <Carousel.Item>
         <img className="d-block w-100" src={banner1} alt="First slide" />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={banner2} alt="Second slide" />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
